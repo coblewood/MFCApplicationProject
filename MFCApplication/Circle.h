9705ce6c1cee596e0b419f88dd6c0a73886edab6
coblewood <coblewoodtwcg@gmail.com>
2025-06-08 18:48:07 +0200

    Update Circle model

--- MFCApplication/Circle.h
+++ MFCApplication/Circle.h
@@ -2,8 +2,21 @@
 #include <atltypes.h>
 #include <afx.h>
 class CCircle {
-public:
+private:
   int center_x;
   int center_y;
   int radius;
+
+public:
+  CCircle() : center_x(0), center_y(0), radius(0) {}
+
+  CCircle(int x, int y, int r) : center_x(x), center_y(y), radius(r) {}
+
+  int GetCenterX() const { return center_x; }
+  int GetCenterY() const { return center_y; }
+  int GetRadius()  const { return radius; }
+
+  void SetCenterX(int x) { center_x = x; }
+  void SetCenterY(int y) { center_y = y; }
+  void SetRadius(int r) { radius = r; }
 };
